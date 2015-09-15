467fba34ce8dcde70402e16f164f8198a96f4bc4
Kane <dmccer@gmail.com>
2015-09-15 16:10:34 +0800

    auto increase index

--- src/crawl.js
+++ src/crawl.js
@@ -15,7 +15,9 @@ const CRAWLJS = './built/src/_crawl.js';
 
 class Crawl extends EventEmitter {
 
-  static get selector() { return '[node-type="feed_list"]'; }
+  static get feeds_sel() { return '[node-type="feed_list"]'; }
+  static get pages_sel() { return '.W_pages'; }
+  static get total_sel() { return '.search_rese'; }
 
   constructor(keywords: string) {
     if (!keywords) {
@@ -30,13 +32,20 @@ class Crawl extends EventEmitter {
   }
 
   run() {
-    shell.exec([PHANTOMJS, CRAWLJS, this.url, this.tmp_filename].join(' '), this.callback.bind(this));
+    shell.exec([PHANTOMJS, CRAWLJS].join(' '), this.callback.bind(this));
   }
 
   cfg() {
     this.url = this.build_url();
     this.tmp_filename = this.tmp_file();
     this.data_filename = this.data_file();
+    this.cookie = this.build_cookie();
+
+    fs.writeFileSync(path.join(config.root, './crawl.json'), JSON.stringify({
+      url: this.url,
+      filename: this.tmp_filename,
+      cookie: this.cookie
+    }));
   }
 
   callback() {
@@ -45,7 +54,19 @@ class Crawl extends EventEmitter {
     let $ = cheerio.load(tmp_file_data.html, {
       decodeEntities: false
     });
-    let $feeds = $(Crawl.selector);
+
+    let $pages = $(Crawl.pages_sel);
+    let $total = $(Crawl.total_sel);
+
+    this.max_page_no = parseInt($pages.find('.layer_menu_list li').last().text().replace(/[^\d]/g, ''));
+    console.log('### MAX_PAGE_NO = ' + this.max_page_no);
+
+    fs.writeFileSync(
+      path.join(config.tmpdir, './pagination.html'),
+      $pages.html() + $total.html()
+    );
+
+    let $feeds = $(Crawl.feeds_sel);
     let feed_list = [];
     $feeds.each((i, el) => {
       feed_list.push($(el).html());
@@ -59,9 +80,13 @@ class Crawl extends EventEmitter {
   }
 
   next() {
-    this.page_no++;
+    if (this.page_no === this.max_page_no) {
+      process.exit();
+    }
 
+    this.page_no++;
     this.cfg();
+    this.run();
   }
 
   tmp_file():string {
@@ -73,8 +98,20 @@ class Crawl extends EventEmitter {
   }
 
   build_url():string {
+    // + '&nodup=1' 忽略新浪的筛选
     let page_query = (this.page_no === 1 ? '' : ('&page=' + this.page_no));
     return WEIBO + urlencode(urlencode(this.keywords)) + page_query;
+  }
+
+  build_cookie():string {
+    let cookies = JSON.parse(fs.readFileSync(config.login).toString()).cookies;
+    let cs = [];
+
+    cookies.forEach((cookie) => {
+      cs.push(`${cookie.name}=${cookie.value};`);
+    });
+
+    return cs.join(' ');
   }
 }
 
